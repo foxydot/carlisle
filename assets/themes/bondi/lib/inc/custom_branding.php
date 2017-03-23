042543fdb540c39c3369b14fd871712e48a3b505
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2017-03-23 15:01:42 -0400

    remove from "the_content" to preserve images

--- assets/themes/bondi/lib/inc/custom_branding.php
+++ assets/themes/bondi/lib/inc/custom_branding.php
@@ -1,7 +1,7 @@
 <?php
 //add special styling to NightFINDERtm and SmartVIEWtm
 add_filter('the_title','cf_custom_branding_filter');
-add_filter('the_content','cf_custom_branding_filter');
+//add_filter('the_content','cf_custom_branding_filter');
 
 function cf_custom_branding_filter($content){
     $content = preg_replace('/NightFINDER/i','<span style="text-transform:capitalize">Night</span><span style="text-transform:uppercase">FINDER</span>',$content);
